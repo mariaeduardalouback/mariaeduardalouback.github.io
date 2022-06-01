d54c529395d6a55ed74727d63dedcfea43d7857e
Your Name <you@example.com>
2022-06-01 12:34:16 -0300

    .

--- PC Web/trabalho_2/trabalho.js
+++ PC Web/trabalho_2/trabalho.js
@@ -1,4 +1,9 @@
-function sum( number1, number2 ) {
+function sum(number1, number2) {
     var DECIMAL = 10;
-    return parseInt( number1, DECIMAL ) + parseInt( number2, DECIMAL );
-  }
+    return parseInt(number1, DECIMAL) + parseInt(number2, DECIMAL);
+}
+
+parseInt('10px'); // 10
+parseInt('10px', 10); // 10
+parseInt('0x'); // NaN
+parseInt('0x', 10) // 0
